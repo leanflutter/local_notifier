fd9228887f0248ae8b4f1740406963031a236589
LiJianying <lijy91@foxmail.com>
2022-03-07 09:20:30 +0800

    Add .clang-format

--- linux/local_notifier_plugin.cc
+++ linux/local_notifier_plugin.cc
@@ -5,92 +5,86 @@
 #include <sys/utsname.h>
 
 #include <cstring>
+
 #include <libnotify/notify.h>
 
 #define LOCAL_NOTIFIER_PLUGIN(obj)                                     \
   (G_TYPE_CHECK_INSTANCE_CAST((obj), local_notifier_plugin_get_type(), \
                               LocalNotifierPlugin))
 
-struct _LocalNotifierPlugin
-{
+struct _LocalNotifierPlugin {
   GObject parent_instance;
-  FlPluginRegistrar *registrar;
+  FlPluginRegistrar* registrar;
 };
 
 G_DEFINE_TYPE(LocalNotifierPlugin, local_notifier_plugin, g_object_get_type())
 
 // Gets the window being controlled.
-GtkWindow *get_window(LocalNotifierPlugin *self)
-{
-  FlView *view = fl_plugin_registrar_get_view(self->registrar);
+GtkWindow* get_window(LocalNotifierPlugin* self) {
+  FlView* view = fl_plugin_registrar_get_view(self->registrar);
   if (view == nullptr)
     return nullptr;
 
   return GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
 }
 
-GdkWindow *get_gdk_window(LocalNotifierPlugin *self)
-{
+GdkWindow* get_gdk_window(LocalNotifierPlugin* self) {
   return gtk_widget_get_window(GTK_WIDGET(get_window(self)));
 }
 
-static FlMethodResponse *notify(LocalNotifierPlugin *self,
-                                FlValue *args)
-{
-  const gchar *title = fl_value_get_string(fl_value_lookup_string(args, "title"));
-  const gchar *subtitle = fl_value_get_string(fl_value_lookup_string(args, "subtitle"));
-  // const gchar *body = fl_value_get_string(fl_value_lookup_string(args, "body"));
+static FlMethodResponse* notify(LocalNotifierPlugin* self, FlValue* args) {
+  const gchar* title =
+      fl_value_get_string(fl_value_lookup_string(args, "title"));
+  const gchar* subtitle =
+      fl_value_get_string(fl_value_lookup_string(args, "subtitle"));
+  // const gchar *body = fl_value_get_string(fl_value_lookup_string(args,
+  // "body"));
 
   notify_init("local_notifier");
-  NotifyNotification *n = notify_notification_new(title, subtitle, 0);
+  NotifyNotification* n = notify_notification_new(title, subtitle, 0);
   notify_notification_show(n, 0);
 
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(fl_value_new_bool(true)));
+  return FL_METHOD_RESPONSE(
+      fl_method_success_response_new(fl_value_new_bool(true)));
 }
 
 // Called when a method call is received from Flutter.
 static void local_notifier_plugin_handle_method_call(
-    LocalNotifierPlugin *self,
-    FlMethodCall *method_call)
-{
+    LocalNotifierPlugin* self,
+    FlMethodCall* method_call) {
   g_autoptr(FlMethodResponse) response = nullptr;
 
-  const gchar *method = fl_method_call_get_name(method_call);
-  FlValue *args = fl_method_call_get_args(method_call);
-  if (strcmp(method, "notify") == 0)
-  {
+  const gchar* method = fl_method_call_get_name(method_call);
+  FlValue* args = fl_method_call_get_args(method_call);
+  if (strcmp(method, "notify") == 0) {
     response = notify(self, args);
-  }
-  else
-  {
+  } else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
 
   fl_method_call_respond(method_call, response, nullptr);
 }
 
-static void local_notifier_plugin_dispose(GObject *object)
-{
+static void local_notifier_plugin_dispose(GObject* object) {
   G_OBJECT_CLASS(local_notifier_plugin_parent_class)->dispose(object);
 }
 
-static void local_notifier_plugin_class_init(LocalNotifierPluginClass *klass)
-{
+static void local_notifier_plugin_class_init(LocalNotifierPluginClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = local_notifier_plugin_dispose;
 }
 
-static void local_notifier_plugin_init(LocalNotifierPlugin *self) {}
+static void local_notifier_plugin_init(LocalNotifierPlugin* self) {}
 
-static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
-                           gpointer user_data)
-{
-  LocalNotifierPlugin *plugin = LOCAL_NOTIFIER_PLUGIN(user_data);
+static void method_call_cb(FlMethodChannel* channel,
+                           FlMethodCall* method_call,
+                           gpointer user_data) {
+  LocalNotifierPlugin* plugin = LOCAL_NOTIFIER_PLUGIN(user_data);
   local_notifier_plugin_handle_method_call(plugin, method_call);
 }
 
-void local_notifier_plugin_register_with_registrar(FlPluginRegistrar *registrar)
-{
-  LocalNotifierPlugin *plugin = LOCAL_NOTIFIER_PLUGIN(
+void local_notifier_plugin_register_with_registrar(
+    FlPluginRegistrar* registrar) {
+  LocalNotifierPlugin* plugin = LOCAL_NOTIFIER_PLUGIN(
       g_object_new(local_notifier_plugin_get_type(), nullptr));
 
   plugin->registrar = FL_PLUGIN_REGISTRAR(g_object_ref(registrar));
@@ -98,11 +92,9 @@ void local_notifier_plugin_register_with_registrar(FlPluginRegistrar *registrar)
   g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
   g_autoptr(FlMethodChannel) channel =
       fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
-                            "local_notifier",
-                            FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(channel, method_call_cb,
-                                            g_object_ref(plugin),
-                                            g_object_unref);
+                            "local_notifier", FL_METHOD_CODEC(codec));
+  fl_method_channel_set_method_call_handler(
+      channel, method_call_cb, g_object_ref(plugin), g_object_unref);
 
   g_object_unref(plugin);
 }
--- windows/local_notifier_plugin.cpp
+++ windows/local_notifier_plugin.cpp
@@ -16,144 +16,131 @@
 
 using namespace WinToastLib;
 
-class CustomToastHandler : public IWinToastHandler
-{
-public:
-    void toastActivated() const
-    {
-    }
+class CustomToastHandler : public IWinToastHandler {
+ public:
+  void toastActivated() const {}
 
-    void toastActivated(int actionIndex) const
-    {
-    }
+  void toastActivated(int actionIndex) const {}
 
-    void toastDismissed(WinToastDismissalReason state) const
-    {
-    }
+  void toastDismissed(WinToastDismissalReason state) const {}
 
-    void toastFailed() const
-    {
-    }
+  void toastFailed() const {}
 };
 
-namespace
-{
-    std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>, std::default_delete<flutter::MethodChannel<flutter::EncodableValue>>> channel = nullptr;
+namespace {
+std::unique_ptr<
+    flutter::MethodChannel<flutter::EncodableValue>,
+    std::default_delete<flutter::MethodChannel<flutter::EncodableValue>>>
+    channel = nullptr;
 
-    class LocalNotifierPlugin : public flutter::Plugin
-    {
-    public:
-        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+class LocalNotifierPlugin : public flutter::Plugin {
+ public:
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
-        LocalNotifierPlugin();
+  LocalNotifierPlugin();
 
-        virtual ~LocalNotifierPlugin();
+  virtual ~LocalNotifierPlugin();
 
-    private:
-        flutter::PluginRegistrarWindows *registrar;
+ private:
+  flutter::PluginRegistrarWindows* registrar;
 
-        void LocalNotifierPlugin::_EmitEvent(std::string eventName);
+  void LocalNotifierPlugin::_EmitEvent(std::string eventName);
 
-        HWND LocalNotifierPlugin::GetMainWindow();
-        void LocalNotifierPlugin::Notify(
-            const flutter::MethodCall<flutter::EncodableValue> &method_call,
-            std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  HWND LocalNotifierPlugin::GetMainWindow();
+  void LocalNotifierPlugin::Notify(
+      const flutter::MethodCall<flutter::EncodableValue>& method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-        // Called when a method is called on this plugin's channel from Dart.
-        void HandleMethodCall(
-            const flutter::MethodCall<flutter::EncodableValue> &method_call,
-            std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-    };
+  // Called when a method is called on this plugin's channel from Dart.
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue>& method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+};
 
-    // static
-    void LocalNotifierPlugin::RegisterWithRegistrar(
-        flutter::PluginRegistrarWindows *registrar)
-    {
-        channel =
-            std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-                registrar->messenger(), "local_notifier",
-                &flutter::StandardMethodCodec::GetInstance());
+// static
+void LocalNotifierPlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows* registrar) {
+  channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+      registrar->messenger(), "local_notifier",
+      &flutter::StandardMethodCodec::GetInstance());
 
-        auto plugin = std::make_unique<LocalNotifierPlugin>();
+  auto plugin = std::make_unique<LocalNotifierPlugin>();
 
-        channel->SetMethodCallHandler(
-            [plugin_pointer = plugin.get()](const auto &call, auto result)
-            {
-                plugin_pointer->HandleMethodCall(call, std::move(result));
-            });
+  channel->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto& call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
 
-        registrar->AddPlugin(std::move(plugin));
-    }
+  registrar->AddPlugin(std::move(plugin));
+}
 
-    LocalNotifierPlugin::LocalNotifierPlugin() {}
+LocalNotifierPlugin::LocalNotifierPlugin() {}
 
-    LocalNotifierPlugin::~LocalNotifierPlugin() {}
+LocalNotifierPlugin::~LocalNotifierPlugin() {}
 
-    void LocalNotifierPlugin::_EmitEvent(std::string eventName)
-    {
-        flutter::EncodableMap args = flutter::EncodableMap();
-        args[flutter::EncodableValue("eventName")] = flutter::EncodableValue(eventName);
-        channel->InvokeMethod("onEvent", std::make_unique<flutter::EncodableValue>(args));
-    }
+void LocalNotifierPlugin::_EmitEvent(std::string eventName) {
+  flutter::EncodableMap args = flutter::EncodableMap();
+  args[flutter::EncodableValue("eventName")] =
+      flutter::EncodableValue(eventName);
+  channel->InvokeMethod("onEvent",
+                        std::make_unique<flutter::EncodableValue>(args));
+}
 
-    HWND LocalNotifierPlugin::GetMainWindow()
-    {
-        return ::GetAncestor(registrar->GetView()->GetNativeWindow(), GA_ROOT);
-    }
+HWND LocalNotifierPlugin::GetMainWindow() {
+  return ::GetAncestor(registrar->GetView()->GetNativeWindow(), GA_ROOT);
+}
 
-    void LocalNotifierPlugin::Notify(
-        const flutter::MethodCall<flutter::EncodableValue> &method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
-    {
-        if (!WinToast::isCompatible())
-        {
-            std::wcout << L"Error, your system in not supported!" << std::endl;
-        }
+void LocalNotifierPlugin::Notify(
+    const flutter::MethodCall<flutter::EncodableValue>& method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (!WinToast::isCompatible()) {
+    std::wcout << L"Error, your system in not supported!" << std::endl;
+  }
 
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
-        const flutter::EncodableMap &args = std::get<flutter::EncodableMap>(*method_call.arguments());
-        std::string title = std::get<std::string>(args.at(flutter::EncodableValue("title")));
-        std::string subtitle = std::get<std::string>(args.at(flutter::EncodableValue("subtitle")));
-        std::string body = std::get<std::string>(args.at(flutter::EncodableValue("body")));
+  const flutter::EncodableMap& args =
+      std::get<flutter::EncodableMap>(*method_call.arguments());
+  std::string title =
+      std::get<std::string>(args.at(flutter::EncodableValue("title")));
+  std::string subtitle =
+      std::get<std::string>(args.at(flutter::EncodableValue("subtitle")));
+  std::string body =
+      std::get<std::string>(args.at(flutter::EncodableValue("body")));
 
-        std::wstring appName = converter.from_bytes(title);
-        std::wstring appUserModelID = converter.from_bytes(title);
+  std::wstring appName = converter.from_bytes(title);
+  std::wstring appUserModelID = converter.from_bytes(title);
 
-        WinToast::instance()->setAppName(appName);
-        WinToast::instance()->setAppUserModelId(appUserModelID);
-        WinToast::instance()->initialize();
+  WinToast::instance()->setAppName(appName);
+  WinToast::instance()->setAppUserModelId(appUserModelID);
+  WinToast::instance()->initialize();
 
-        WinToastTemplate toast = WinToastTemplate(WinToastTemplate::Text02);
-        toast.setTextField(converter.from_bytes(subtitle), WinToastTemplate::FirstLine);
-        toast.setTextField(converter.from_bytes(body), WinToastTemplate::SecondLine);
+  WinToastTemplate toast = WinToastTemplate(WinToastTemplate::Text02);
+  toast.setTextField(converter.from_bytes(subtitle),
+                     WinToastTemplate::FirstLine);
+  toast.setTextField(converter.from_bytes(body), WinToastTemplate::SecondLine);
 
-        CustomToastHandler *handler = new CustomToastHandler();
-        WinToast::instance()->showToast(toast, handler);
+  CustomToastHandler* handler = new CustomToastHandler();
+  WinToast::instance()->showToast(toast, handler);
 
-        result->Success(flutter::EncodableValue(true));
-    }
+  result->Success(flutter::EncodableValue(true));
+}
 
-    void LocalNotifierPlugin::HandleMethodCall(
-        const flutter::MethodCall<flutter::EncodableValue> &method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
-    {
-        if (method_call.method_name().compare("notify") == 0)
-        {
-            Notify(method_call, std::move(result));
-        }
-        else
-        {
-            result->NotImplemented();
-        }
-    }
+void LocalNotifierPlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue>& method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("notify") == 0) {
+    Notify(method_call, std::move(result));
+  } else {
+    result->NotImplemented();
+  }
+}
 
-} // namespace
+}  // namespace
 
 void LocalNotifierPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar)
-{
-    LocalNotifierPlugin::RegisterWithRegistrar(
-        flutter::PluginRegistrarManager::GetInstance()
-            ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+    FlutterDesktopPluginRegistrarRef registrar) {
+  LocalNotifierPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
