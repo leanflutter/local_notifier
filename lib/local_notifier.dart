add864caaf303642ab6bd9fab69f439d29dd05e6
n.morozov <n.morozov@teleguard.com>
2022-08-12 11:13:29 +0300

    Now is able to pass toast's duration (short is about 5 sec) and image path (when it's null, using Text02, either ImageAndText02)

--- lib/local_notifier.dart
+++ lib/local_notifier.dart
@@ -1,5 +1,6 @@
 export 'src/local_notification_action.dart';
 export 'src/local_notification_close_reason.dart';
+export 'src/local_notification_duration.dart';
 export 'src/local_notification_listener.dart';
 export 'src/local_notification.dart';
 export 'src/local_notifier.dart';
